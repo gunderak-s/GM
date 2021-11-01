43e4defaf15efb41ea26eb3b58a190d1842717a2
oreletska <olena482fly@gmail.com>
2021-11-01 19:14:51 +0200

    + поле гри

---
+++ src/com/company/StreetCellGame.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class StreetCellGame extends CellGame {
+    int priceCells;
+    int statusRenta;
+    int priceHouse;
+    int priceHotel;
+    int[] rentaMas;
+    Player holder;
+    StreetCellGame(int sr, int ph, int pht,int rm, Player h) {
+
+    }
+}
+
--- src/com/company/TestGameM.java
+++ src/com/company/TestGameM.java
@@ -3,7 +3,7 @@ package com.company;
 public class TestGameM {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("\u001B[32m"+(char)8962);
+        // write your code here
+        System.out.println("\u001B[32m" + (char) 8962);
     }
 }
