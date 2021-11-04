ca2e860547ceaed9d6c84ca231500360dc36a1bb
oreletska <olena482fly@gmail.com>
2021-11-04 18:58:36 +0200

    + дороблено конструктор і дорога гри

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -5,9 +5,16 @@ public class Player {
     char symbol;
     int money;
     int positionOnRoad;
+    {positionOnRoad=0;}
 
     void setPositionOnRoad(int cube){
-
+        for (int i = 0; i < cube; i++) {
+            if (positionOnRoad!=40){
+            positionOnRoad++;}
+            else {
+                positionOnRoad=0;
+            }
+        }
     }
 
 }
