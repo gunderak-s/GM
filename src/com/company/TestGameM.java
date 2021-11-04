1a47b05ffc0a515589dfa9d7de2a67edc6f46282
Sasha Gunderak <o.gunderak@gmail.com>
2021-11-04 23:02:48 +0200

    виправлення назв і оформлення

--- src/com/company/TestGameM.java
+++ src/com/company/TestGameM.java
@@ -5,7 +5,7 @@ public class TestGameM {
     public static void main(String[] args) {
 	// write your code here
         GameM g1=new GameM();
-        g1.setFild();
+        g1.setField();
         g1.Print();
     }
 }
