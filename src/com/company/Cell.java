fb8e1c35427465f7d92e142e0ec832b18badf10d
VladimirKivg <cheremuha1@gmal.com>
2021-11-11 18:46:13 +0200

    змінив назву одного методу, і прописав те що робили на уроці

--- src/com/company/Cell.java
+++ src/com/company/Cell.java
@@ -1,40 +1,61 @@
 package com.company;
 
-import java.io.Serializable;
-
-public class Cell implements Serializable {
+public class Cell {
+    int rotation;
     String[][] cellsMatrix;
-    final static int SIZE_CELLS =4;
+    final static int SIZE_CELLS = 4;
+
     //##################################################################################################################################################
-    Cell(){
-        cellsMatrix = new String[][]{{" ", " ", " ", " "},
+    Cell() {
+        cellsMatrix = new String[][]{
+                {" ", " ", " ", " "},
                 {" ", " ", " ", " "},
                 {" ", " ", " ", " "},
                 {" ", " ", " ", " "}
         };
     }
+
     //##################################################################################################################################################
-    void action(Player player){
-        redrawSymbolPlayer(" ",player.symbol);
-    }
-    //##################################################################################################################################################
-    void redrawSymbolPlayer(String prevChar, String newChar){
-                for (int i=0;i<4;i++)
-                    for (int j=0;j<4;j++)
-                    if (cellsMatrix[i][j]==prevChar) {
-                        cellsMatrix[i][j] = newChar;
-                        return;
-                    }
-    }
-    //##################################################################################################################################################
-    void  redrawStatus(char holderChar){
+    void action(Player player) {
 
     }
+
     //##################################################################################################################################################
-    void printInfo(){
+    void redrawSymbolPlayer(String oldChar, String newChar) {
+        //0-знизу, 1-зліва, 2-згори, 3-справа
+        switch (rotation) {
+            case 2:
+            for (int i = 0; i < cellsMatrix.length; i++) {
+                if (cellsMatrix[2][i] == oldChar) cellsMatrix[2][i] = newChar;
+            }break;
+            case 3:
+                for (int i = 0; i < cellsMatrix.length; i++) {
+                    if (cellsMatrix[i][1] == oldChar) cellsMatrix[i][1] = newChar;
+                }break;
+            case 0:
+                for (int i = 0; i < cellsMatrix.length; i++) {
+                    if (cellsMatrix[1][i] == oldChar) cellsMatrix[1][i] = newChar;
+                }break;
+            case 1:
+                for (int i = 0; i < cellsMatrix.length; i++) {
+                    if (cellsMatrix[i][2] == oldChar) cellsMatrix[i][2] = newChar;
+                }break;
+
+        }
+
     }
+
     //##################################################################################################################################################
-    void printMatrix(int row){
-        System.out.print(GameM.WHITE_BACKGROUND+cellsMatrix[row][0]+cellsMatrix[row][1]+cellsMatrix[row][2]+cellsMatrix[row][3]);
+    void redrawStatus(char holderChar) {
+
+    }
+
+    //##################################################################################################################################################
+    void printInfo() {
+    }
+
+    //##################################################################################################################################################
+    void printMatrix(int row) {
+        System.out.print(GameM.WHITE_BACKGROUND + cellsMatrix[row][0] + cellsMatrix[row][1] + cellsMatrix[row][2] + cellsMatrix[row][3]);
     }
 }
--- src/com/company/CityCell.java
+++ src/com/company/CityCell.java
@@ -11,7 +11,6 @@ public class CityCell extends Cell {
     Cell helpful;
     //##################################################################################################################################################
     CityCell(String name, int rotation,  String color, int statusRenta, int priceHouse , int priceHotel, Player holder,Cell helpful, int ... rentaMas) {
-        //super();
         this.rotation=rotation;
         this.color=color;
         this.statusRenta=statusRenta;
@@ -31,47 +30,21 @@ public class CityCell extends Cell {
             player.money =player.money -rentaMas[statusRenta];
 
         }
-        redrawSymbolPlayer(" ", player.symbol);
-    }
-    //##################################################################################################################################################
-    void redrawSymbolPlayer(String prevChar,String newChar){
-        switch (rotation) {
-            case 0:
-                for (int i=0;i<4;i++)
-                    if (cellsMatrix[2][i]==prevChar){
-                        cellsMatrix[2][i]=newChar;
-                        return;
-                    }
-            case 1:
-                for (int i=0;i<4;i++)
-                    if (cellsMatrix[i][1]==prevChar){
-                        cellsMatrix[i][1]=newChar;
-                        return;
-                    }
-            case 2:
-                for (int i=0;i<4;i++)
-                    if (cellsMatrix[1][i]==prevChar){
-                        cellsMatrix[1][i]=newChar;
-                        return;
-                    }
-            case 3:
-                for (int i=0;i<4;i++)
-                    if (cellsMatrix[i][2]==prevChar){
-                        cellsMatrix[i][2]=newChar;
-                        return;
-                    }
 
-        }
     }
     //##################################################################################################################################################
-    void  redrawStatus(String holderChar){
+    void redrawSymbolPlayer(String oldChar, String newChar){
+
+    }
+    //##################################################################################################################################################
+    void  redrawStatus(char holderChar){
         //--------------------------------------------------Запис власника-------------------------------------------------------------------------------
 
         //--------------------------------------------------Зміна ренти-------------------------------------------------------------------------------
         int price =rentaMas[statusRenta];
         String s=String.valueOf(price);
-        String[] masS=s.split("");
-        //int lengthMas=masC.length;
+        char[] masC=s.toCharArray();
+        int lengthMas=masC.length;
 
         //masC[0]=2
         //masC[1]=8
