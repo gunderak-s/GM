3f73b1c6dab39ef40f9f863039f870d0b4f99ef0
VladimirKivg <cheremuha1@gmal.com>
2021-11-11 18:46:13 +0200

    змінив назву одного методу, і прописав те що робили на уроці

--- src/com/company/Cell.java
+++ src/com/company/Cell.java
@@ -1,33 +1,61 @@
 package com.company;
 
 public class Cell {
+    int rotation;
     String[][] cellsMatrix;
-    final static int SIZE_CELLS =4;
+    final static int SIZE_CELLS = 4;
+
     //##################################################################################################################################################
-    Cell(){
-        cellsMatrix = new String[][]{{" ", " ", " ", " "},
+    Cell() {
+        cellsMatrix = new String[][]{
+                {" ", " ", " ", " "},
                 {" ", " ", " ", " "},
                 {" ", " ", " ", " "},
                 {" ", " ", " ", " "}
         };
     }
+
     //##################################################################################################################################################
-    void action(Player player){
+    void action(Player player) {
 
     }
+
     //##################################################################################################################################################
-    void redrawSymbolPlayer(String prevChar, String newChar){
+    void redrawSymbolPlayer(String oldChar, String newChar) {
+        //0-знизу, 1-зліва, 2-згори, 3-справа
+        switch (rotation) {
+            case 2:
+            for (int i = 0; i < cellsMatrix.length; i++) {
+                if (cellsMatrix[2][i] == oldChar) cellsMatrix[2][i] = newChar;
+            }break;
+            case 3:
+                for (int i = 0; i < cellsMatrix.length; i++) {
+                    if (cellsMatrix[i][1] == oldChar) cellsMatrix[i][1] = newChar;
+                }break;
+            case 0:
+                for (int i = 0; i < cellsMatrix.length; i++) {
+                    if (cellsMatrix[1][i] == oldChar) cellsMatrix[1][i] = newChar;
+                }break;
+            case 1:
+                for (int i = 0; i < cellsMatrix.length; i++) {
+                    if (cellsMatrix[i][2] == oldChar) cellsMatrix[i][2] = newChar;
+                }break;
+
+        }
 
     }
+
     //##################################################################################################################################################
-    void  redrawStatus(char holderChar){
+    void redrawStatus(char holderChar) {
 
     }
+
     //##################################################################################################################################################
-    void printInfo(){
+    void printInfo() {
     }
+
     //##################################################################################################################################################
-    void printMatrix(int row){
-        System.out.print(GameM.WHITE_BACKGROUND+cellsMatrix[row][0]+cellsMatrix[row][1]+cellsMatrix[row][2]+cellsMatrix[row][3]);
+    void printMatrix(int row) {
+        System.out.print(GameM.WHITE_BACKGROUND + cellsMatrix[row][0] + cellsMatrix[row][1] + cellsMatrix[row][2] + cellsMatrix[row][3]);
     }
 }
